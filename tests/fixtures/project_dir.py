fb88af360a9057e46a4114e6d5849fdc5d602901
Vinicius Lopes Alvarenga <viniciusl.alvarenga@hotmail.com>
2025-11-06 12:37:31 -0300

    add try-finally to always clean the test

--- tests/fixtures/project_dir.py
+++ tests/fixtures/project_dir.py
@@ -19,8 +19,10 @@ def project_dir() -> Generator[Path, None, None]:
         "repo_name": "test_repo",
     }
     generated_proj_dir: Path = generate_project(template_value=template_values)
-    initialize_git_repo(generated_proj_dir)
-    # Run linting to fix any formatting issues in the generated project antes dos testes com o mesmo commando
-    subprocess.run(["make", "lint-ci"], cwd=generated_proj_dir, check=False)
-    yield generated_proj_dir
-    shutil.rmtree(path=generated_proj_dir)
+    try:
+        initialize_git_repo(generated_proj_dir)
+        # Run linting to fix any formatting issues in the generated project antes dos testes com o mesmo commando
+        subprocess.run(["make", "lint-ci"], cwd=generated_proj_dir, check=False)
+        yield generated_proj_dir
+    finally:
+        shutil.rmtree(path=generated_proj_dir)
