6caa17b4e65d74b49659b6ccd1632bbabd90bac4
Vinicius Lopes Alvarenga <viniciusl.alvarenga@hotmail.com>
2025-11-06 11:08:12 -0300

    fix lint issues

--- {{cookiecutter.repo_name}}/tests/test_states_info.py
+++ {{cookiecutter.repo_name}}/tests/test_states_info.py
@@ -1,9 +1,6 @@
 """Tests for `example_pkg.states_info`."""
 
-from pathlib import Path
-
 import pytest
-
 from example_pkg.states_info import (
     is_city_capitol_of_state,
     slow_add,
@@ -20,6 +17,7 @@ from example_pkg.states_info import (
     ],
 )
 def test__is_city_capitol_of_state(city_name: str, state: str, is_capitol: bool):
+    """Test `is_city_capitol_of_state()`."""
     assert is_city_capitol_of_state(city_name=city_name, state=state) == is_capitol
 
 
